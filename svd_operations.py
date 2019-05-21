bfac18707fd579a411998beba491cea819b81574
Kieran Bull <pykb@localhost.localdomain>
2019-05-21 22:05:41 +0100

    added D_cap for svd compression

--- svd_operations.py
+++ svd_operations.py
@@ -9,6 +9,10 @@ class svd_node_pair:
             reshaped_node = node1.tensor.reshape(np.array((shape[0] * shape[1],shape[2])))
 
             A,S,Vh = linalg.svd(reshaped_node,full_matrices=False)
+            if D_cap is not None:
+                A = A[:,0:D_cap]
+                S = S[0:D_cap]
+                Vh = Vh[0:D_cap,:]
             #rescale S
             S = S / np.max(S)
             new_left_node = A.reshape((shape[0],shape[1],np.size(S)))
@@ -20,6 +24,10 @@ class svd_node_pair:
             reshaped_node = node1.tensor.reshape(np.array((shape[0] * shape[1],shape[2])))
 
             A,S,Vh = np.linalg.svd(reshaped_node,full_matrices=False)
+            if D_cap is not None:
+                A = A[:,0:D_cap]
+                S = S[0:D_cap]
+                Vh = Vh[0:D_cap,:]
             S = S / np.max(S)
             A = A.reshape((shape[0],shape[1],np.size(S)))
             M = np.einsum('ij,jk,uk->ui',np.diag(S),Vh,node2.tensor)
@@ -27,6 +35,10 @@ class svd_node_pair:
 
         elif node1.legs == "right" and node2.legs == "both":
             A,S,Vh = np.linalg.svd(node1.tensor,full_matrices=False)
+            if D_cap is not None:
+                A = A[:,0:D_cap]
+                S = S[0:D_cap]
+                Vh = Vh[0:D_cap,:]
             S = S / np.max(S)
             new_left_node = A
             new_right_node = np.einsum('ij,jk,ukl->uil',np.diag(S),Vh,node2.tensor)
@@ -79,43 +91,36 @@ class svd_node_pair:
             new_left_node = np.einsum('ij,jk,kl->il',node1.tensor,U,np.diag(S))
             return new_left_node, new_right_node
 
-def svd_norm_node(node,D_cap=None,norm_dir=None):
-    if node.legs == "left":
-        shape = np.shape(node.tensor)
-        M = node.tensor.transpose()
-        M = M.reshape(np.array((1,shape[0]*shape[1])))
-        U,S,B = np.linalg.svd(M,full_matrices=False)
-        if D_cap is not None:
-            B=B[0:D_cap,:]
-            B=B.reshape(np.array((D_cap,shape[0])))
-        else:
-            B=B.reshape(np.array((shape[1],shape[0])))
-        return B.transpose()
-
-    elif node.legs == "right":
-        shape = np.shape(node.tensor)
-        M = node.tensor.reshape(np.array((shape[0]*shape[1],1)))
-        A,S,Vh = np.linalg.svd(M,full_matrices=False)
-        if D_cap is not None:
-            A = A[:,0:D_Cap]
-            A = A.reshape(np.array((shape[0],D_cap)))
-        else:
-            A = A.reshape(np.array((shape[0],shape[1])))
-        return A
-
-    elif node.legs == "both":
-        if norm_dir == "left":
+class svd_norm_node:
+    def left(node,D_cap=None):
+        if node.legs == "both":
             shape = np.shape(node.tensor)
             M = node.tensor.reshape(np.array((shape[0]*shape[1],shape[2])))
             A,S,Vh = np.linalg.svd(M,full_matrices=False)
             A = np.reshape(A,np.array((shape[0],shape[1],shape[2])))
             return A
-        elif norm_dir == "right":
-            shape = np.shape(node.tensor)
-            reshaped_node = np.reshape(np.einsum('ijk->jki',node.tensor),np.array((shape[1],shape[0]*shape[2])))
 
-            U,S,B = np.linalg.svd(reshaped_node,full_matrices=False)
+        else:
+            shape = np.shape(node.tensor)
+            M = node.tensor.reshape(np.array((shape[0]*shape[1],1)))
+            A,S,Vh = np.linalg.svd(M,full_matrices=False)
+            A = np.reshape(A,np.array((shape[0],shape[1])))
+            return A
+
+    def right(node,D_cap=None):
+        if node.legs == "both":
+            shape = np.shape(node.tensor)
+            M = np.reshape(np.einsum('ijk->jki',node.tensor),np.array((shape[1],shape[0]*shape[2])))
+            U,S,B = np.linalg.svd(M,full_matrices=False)
             B = np.reshape(B,np.array((np.size(S),shape[2],shape[0])))
             B = np.einsum('ijk->kij',B)
             return B
+
+        else:
+            shape = np.shape(node.tensor)
+            M = node.tensor.transpose()
+            M = M.reshape(np.array((1,shape[0]*shape[1])))
+            U,S,B = np.linalg.svd(M,full_matrices=False)
+            B = np.reshape(B,np.array((shape[1],shape[0])))
+            return B.transpose()
 
