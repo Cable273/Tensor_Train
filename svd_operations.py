a7b1ba43a65ff8711cf4ba56998316cb5fb07273
Kieran Bull <pykb@localhost.localdomain>
2019-05-23 00:07:45 +0100

    add keyword argument for rescale. To try alleviate inf/Nan at large N, still an issue

--- svd_operations.py
+++ svd_operations.py
@@ -3,12 +3,14 @@
 import numpy as np
 import scipy.linalg as linalg
 class svd_node_pair:
-    def left(node1,node2,D_cap=None):
+    def left(node1,node2,D_cap=None,rescale=False):
         if node1.legs == "both" and node2.legs == "both" : 
             shape = np.shape(node1.tensor)
             reshaped_node = node1.tensor.reshape(np.array((shape[0] * shape[1],shape[2])))
 
             A,S,Vh = linalg.svd(reshaped_node,full_matrices=False)
+            if rescale is not True:
+                S = S / np.max(S)
             if D_cap is not None:
                 A = A[:,0:D_cap]
                 S = S[0:D_cap]
@@ -21,7 +23,9 @@ class svd_node_pair:
             shape = np.shape(node1.tensor)
             reshaped_node = node1.tensor.reshape(np.array((shape[0] * shape[1],shape[2])))
 
-            A,S,Vh = np.linalg.svd(reshaped_node,full_matrices=False)
+            A,S,Vh = linalg.svd(reshaped_node,full_matrices=False)
+            if rescale is True:
+                S = S / np.max(S)
             if D_cap is not None:
                 A = A[:,0:D_cap]
                 S = S[0:D_cap]
@@ -31,22 +35,25 @@ class svd_node_pair:
             return A,M
 
         elif node1.legs == "right" and node2.legs == "both":
-            A,S,Vh = np.linalg.svd(node1.tensor,full_matrices=False)
+            A,S,Vh = linalg.svd(node1.tensor,full_matrices=False)
+            if rescale is True:
+                S = S / np.max(S)
             if D_cap is not None:
                 A = A[:,0:D_cap]
                 S = S[0:D_cap]
                 Vh = Vh[0:D_cap,:]
-            S = S / np.max(S)
             new_left_node = A
             new_right_node = np.einsum('ij,jk,ukl->uil',np.diag(S),Vh,node2.tensor)
             return new_left_node, new_right_node
 
-    def right(node1,node2,D_cap=None):
+    def right(node1,node2,D_cap=None,rescale=False):
         if node1.legs == "both" and node2.legs == "both" : 
             shape = np.shape(node2.tensor)
             reshaped_node = np.reshape(np.einsum('ijk->jki',node2.tensor),np.array((shape[1],shape[0]*shape[2])))
 
-            U,S,B = np.linalg.svd(reshaped_node,full_matrices=False)
+            U,S,B = linalg.svd(reshaped_node,full_matrices=False)
+            if rescale is True:
+                S = S / np.max(S)
             if D_cap is not None:
                 U = U[:,0:D_cap]
                 S = S[0:D_cap]
@@ -60,7 +67,9 @@ class svd_node_pair:
             shape = np.shape(node2.tensor)
             reshaped_node = node2.tensor.transpose()
 
-            U,S,B = np.linalg.svd(reshaped_node,full_matrices=False)
+            U,S,B = linalg.svd(reshaped_node,full_matrices=False)
+            if rescale is True:
+                S = S / np.max(S)
             if D_cap is not None:
                 U = U[:,0:D_cap]
                 S = S[0:D_cap]
@@ -73,7 +82,9 @@ class svd_node_pair:
             shape = np.shape(node2.tensor)
             reshaped_node = np.reshape(np.einsum('ijk->jki',node2.tensor),np.array((shape[1],shape[0]*shape[2])))
 
-            U,S,B = np.linalg.svd(reshaped_node,full_matrices=False)
+            U,S,B = linalg.svd(reshaped_node,full_matrices=False)
+            if rescale is True:
+                S = S / np.max(S)
             if D_cap is not None:
                 U = U[:,0:D_cap]
                 S = S[0:D_cap]
@@ -90,14 +101,14 @@ class svd_norm_node:
         if node.legs == "both":
             shape = np.shape(node.tensor)
             M = node.tensor.reshape(np.array((shape[0]*shape[1],shape[2])))
-            A,S,Vh = np.linalg.svd(M,full_matrices=False)
+            A,S,Vh = linalg.svd(M,full_matrices=False)
             A = np.reshape(A,np.array((shape[0],shape[1],shape[2])))
             return A
 
         else:
             shape = np.shape(node.tensor)
             M = node.tensor.reshape(np.array((shape[0]*shape[1],1)))
-            A,S,Vh = np.linalg.svd(M,full_matrices=False)
+            A,S,Vh = linalg.svd(M,full_matrices=False)
             A = np.reshape(A,np.array((shape[0],shape[1])))
             return A
 
@@ -105,7 +116,7 @@ class svd_norm_node:
         if node.legs == "both":
             shape = np.shape(node.tensor)
             M = np.reshape(np.einsum('ijk->jki',node.tensor),np.array((shape[1],shape[0]*shape[2])))
-            U,S,B = np.linalg.svd(M,full_matrices=False)
+            U,S,B = linalg.svd(M,full_matrices=False)
             B = np.reshape(B,np.array((np.size(S),shape[2],shape[0])))
             B = np.einsum('ijk->kij',B)
             return B
@@ -114,7 +125,7 @@ class svd_norm_node:
             shape = np.shape(node.tensor)
             M = node.tensor.transpose()
             M = M.reshape(np.array((1,shape[0]*shape[1])))
-            U,S,B = np.linalg.svd(M,full_matrices=False)
+            U,S,B = linalg.svd(M,full_matrices=False)
             B = np.reshape(B,np.array((shape[1],shape[0])))
             return B.transpose()
 
