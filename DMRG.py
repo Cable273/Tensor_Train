61070fb1bc4e7af59ed9a2ea960a24adf4967e9d
Kieran Bull <pykb@localhost.localdomain>
2019-05-23 17:57:07 +0100

    infinte dmrg

--- DMRG.py
+++ DMRG.py
@@ -84,18 +84,30 @@ class dmrg:
             new_phys_dim = int(np.size(M)/(shape_A[1]*shape_A[2]))
             M = M.reshape(np.array((new_phys_dim,shape_A[1],shape_A[2])))
 
-            #update site and svd to shift norm
+            #update site 
             self.psi.node[n].tensor = M
+
+            #update errors
+            clayer = collapsed_layer.factory(layer(self.network,n))
+            clayer = combine_collapsed_layers.new_collapsed_layer(self.L[n-1],clayer)
+            self.energy = combine_collapsed_layers.scalar(clayer,self.R[n+1])
+            self.energy_vals = np.append(self.energy_vals,self.energy)
+            clayer = collapsed_layer.factory(layer(self.var_network,n))
+            clayer = combine_collapsed_layers.new_collapsed_layer(self.var_L[n-1],clayer)
+            self.variance = combine_collapsed_layers.scalar(clayer,self.var_R[n+1]) - self.energy**2
+            self.variance_vals = np.append(self.variance_vals,self.variance)
+
+            #shift norm
             self.psi.node[n].tensor, self.psi.node[n+1].tensor = svd_node_pair.left(self.psi.node[n],self.psi.node[n+1])
 
+            #form new L blocks
             clayer = collapsed_layer.factory(layer(self.network,n))
             self.L[n] = combine_collapsed_layers.new_collapsed_layer(self.L[n-1],clayer)
             clayer = collapsed_layer.factory(layer(self.var_network,n))
             self.var_L[n] = combine_collapsed_layers.new_collapsed_layer(self.var_L[n-1],clayer)
 
+
         self.psi.node[self.length-1].tensor = svd_norm_node.left(self.psi.node[self.length-1])
-        # self.network.contract()
-        # self.energy = self.network.contraction
 
         clayer = collapsed_layer.factory(layer(self.network,self.length-1))
         self.energy = combine_collapsed_layers.scalar(self.L[self.length-2],clayer)
@@ -138,15 +150,29 @@ class dmrg:
             new_phys_dim = int(np.size(M)/(shape_A[1]*shape_A[2]))
             M = M.reshape(np.array((new_phys_dim,shape_A[1],shape_A[2])))
 
-            #update site and svd to shift norm
+            #update site
             self.psi.node[n].tensor = M
+
+            #update errors
+            clayer = collapsed_layer.factory(layer(self.network,n))
+            clayer = combine_collapsed_layers.new_collapsed_layer(self.L[n-1],clayer)
+            self.energy = combine_collapsed_layers.scalar(clayer,self.R[n+1])
+            # print("\n\n"+str(self.energy))
+            self.energy_vals = np.append(self.energy_vals,self.energy)
+            clayer = collapsed_layer.factory(layer(self.var_network,n))
+            clayer = combine_collapsed_layers.new_collapsed_layer(self.var_L[n-1],clayer)
+            self.variance = combine_collapsed_layers.scalar(clayer,self.var_R[n+1]) - self.energy**2
+            self.variance_vals = np.append(self.variance_vals,self.variance)
+
+            #shift norm
             self.psi.node[n-1].tensor, self.psi.node[n].tensor = svd_node_pair.right(self.psi.node[n-1],self.psi.node[n])
 
+            #update R blocks
             clayer = collapsed_layer.factory(layer(self.network,n))
             self.R[n] = combine_collapsed_layers.new_collapsed_layer(clayer,self.R[n+1])
-
             clayer = collapsed_layer.factory(layer(self.var_network,n))
             self.var_R[n] = combine_collapsed_layers.new_collapsed_layer(clayer,self.var_R[n+1])
+
 
         self.psi.node[0].tensor = svd_norm_node.right(self.psi.node[0])
 
@@ -212,3 +238,126 @@ class dmrg:
         plt.title(r"$\textrm{DMRG Sweep variance}(H)$")
         plt.show()
 
+class idmrg:
+    #init with 4 site MPO + g 
+    def __init__(self,H_4site,phys_dim,D):
+        self.D = D
+        print(self.D)
+        method = dmrg(H_4site,self.D)
+        self.psi =  method.run()
+        print(np.shape(self.psi.node[1].tensor))
+
+        self.psi_grown = copy.deepcopy(self.psi)
+        self.H_4site = H_4site
+        self.H_grown = copy.deepcopy(self.H_4site)
+        self.phys_dim = phys_dim
+        self.length = self.psi.length
+
+        #fix norm AASBB
+        self.psi.node[0].tensor, self.psi.node[1].tensor = svd_node_pair.left(self.psi.node[0],self.psi.node[1],D_cap=self.D)
+        self.psi.node[2].tensor,self.psi.node[3].tensor = svd_node_pair.right(self.psi.node[2],self.psi.node[3],D_cap=self.D)
+
+        T = np.einsum('ijk,ukv->ijuv',self.psi.node[1].tensor,self.psi.node[2].tensor)
+        shape0 = np.shape(T)
+        T = T.reshape((np.array((shape0[0]*shape0[1],shape0[2]*shape0[3]))))
+        
+        U,S,Vh = np.linalg.svd(T,full_matrices=False)
+        U=U[:,0:self.D]
+        S=S[0:self.D]
+        Vh=Vh[0:self.D,:]
+        U = np.dot(U,np.power(np.diag(S),0.5))
+        Vh = np.dot(np.power(np.diag(S),0.5),Vh)
+
+        A = U.reshape(np.array((shape0[0],shape0[1],np.size(S))))
+
+        Vh = Vh.reshape(np.array((np.size(S),shape0[2],shape0[3])))
+        B = np.einsum('ijk->jik',Vh)
+        shape = np.shape(B)
+        B = B.reshape(np.array((shape[0],shape[1],shape[2])))
+
+        self.psi.node[1].tensor = A
+        self.psi.node[2].tensor = B
+
+
+        #form L=AA, R=BB blocks for efficient H construction
+        self.network = rail_network(self.psi_grown,self.psi_grown,self.H_4site)
+        clayer0 = collapsed_layer.factory(layer(self.network,0))
+        clayer1 = collapsed_layer.factory(layer(self.network,1))
+        self.L = combine_collapsed_layers.new_collapsed_layer(clayer0,clayer1)
+
+        clayer2 = collapsed_layer.factory(layer(self.network,2))
+        clayer3 = collapsed_layer.factory(layer(self.network,3))
+        self.R = combine_collapsed_layers.new_collapsed_layer(clayer2,clayer3)
+
+    #add two sites to centre of chain, optimize and convert to mps nodes
+    def grow(self):
+        #add two sites
+        psi_grown = open_MPS(self.length+2)
+        H_grown = open_MPO(psi_grown.length)
+        psi_grown.set_entry(0,self.psi_grown.node[0].tensor,"right")
+        H_grown.set_entry(0,self.H_grown.node[0].tensor,"right")
+        c=1
+        for n in range(1,int(self.length/2)):
+            psi_grown.set_entry(c,self.psi_grown.node[n].tensor,"both")
+            H_grown.set_entry(c,self.H_grown.node[n].tensor,"both")
+            c=c+1
+
+        c=int(psi_grown.length/2+1)
+        for n in range(int(self.length/2),self.length-1):
+            psi_grown.set_entry(c,self.psi_grown.node[n].tensor,"both")
+            H_grown.set_entry(c,self.H_grown.node[n].tensor,"both")
+            c=c+1
+        psi_grown.set_entry(psi_grown.length-1,self.psi_grown.node[self.psi_grown.length-1].tensor,"left")
+        H_grown.set_entry(H_grown.length-1,self.H_grown.node[self.H_grown.length-1].tensor,"both")
+
+        #construct eigenvalue prob to optimize 2 sites
+        H_opt = np.einsum('abc,gjbi,hkie,def->ghadjkcf',self.L.tensor,self.H_4site.node[1].tensor,self.H_4site.node[2].tensor,self.R.tensor)
+        shape = np.shape(H_opt)
+        H_opt = H_opt.reshape(shape[0]*shape[1]*shape[2]*shape[3],shape[4]*shape[5]*shape[6]*shape[7])
+        e,u = np.linalg.eigh(H_opt)
+        M = u[:,0]
+        L_shape = np.shape(self.L.tensor)
+        R_shape = np.shape(self.R.tensor)
+        W_shape = np.shape(self.H_4site.node[1].tensor)
+
+        M = M.reshape(W_shape[0],W_shape[0],L_shape[0],R_shape[0])
+
+        #reshape node pair to two mps nodes
+        M = np.einsum('ijku->ikju',M)
+        shape = np.shape(M)
+        M = M.reshape(np.array((shape[0]*shape[1],shape[2]*shape[3])))
+        A,S,B = np.linalg.svd(M,full_matrices=False)
+        A=A[:,0:self.D]
+        S=S[0:self.D]
+        B=B[0:self.D,:]
+        A = np.dot(A,np.power(np.diag(S),0.5))
+        B = np.dot(np.power(np.diag(S),0.5),B)
+
+        A = A.reshape(np.array((shape[0],shape[1],np.size(S))))
+
+        B = B.reshape(np.array((np.size(S),shape[2],shape[3])))
+        B = np.einsum('ijk->jik',B)
+
+        index = int(psi_grown.length/2-1)
+        psi_grown.set_entry(index,A,"both")
+        psi_grown.set_entry(index+1,B,"both")
+        H_grown.set_entry(index,self.H_4site.node[1].tensor,"both")
+        H_grown.set_entry(index+1,self.H_4site.node[2].tensor,"both")
+        self.H_grown = H_grown
+
+        self.psi_grown = psi_grown
+        self.length = self.psi_grown.length
+
+        self.network = rail_network(self.psi_grown,self.psi_grown,H_grown)
+        clayer = collapsed_layer.factory(layer(self.network,index))
+        self.L = combine_collapsed_layers.new_collapsed_layer(self.L,clayer)
+
+        clayer = collapsed_layer.factory(layer(self.network,index+1))
+        self.R = combine_collapsed_layers.new_collapsed_layer(clayer,self.R)
+
+    def run(self,N):
+        print("Infinite DMRG to L="+str(N))
+        pbar=ProgressBar()
+        for n in pbar(range(0,int((N-4)/2))):
+            self.grow()
+        return self.psi_grown
--- dmrg_run.py
+++ dmrg_run.py
@@ -6,9 +6,34 @@ from Tensor_Train import rail_network
 from common_MPOs import common_mpo
 from compression import var_compress,svd_compress
 import matplotlib.pyplot as plt
-from DMRG import dmrg
-N=147
-D=4
+from DMRG import *
+
+import sys
+file_dir = '/localhome/pykb/physics_code/Exact_Diagonalization/Classes/'
+sys.path.append(file_dir)
+file_dir = '/localhome/pykb/physics_code/Exact_Diagonalization/functions/'
+sys.path.append(file_dir)
+
+from Hamiltonian_Classes import Hamiltonian,H_table,clock_Hamiltonian,spin_Hamiltonian
+from System_Classes import unlocking_System
+from Symmetry_Classes import translational,parity,model_sym_data,charge_conjugation
+# from Plotting_Classes import eig_overlap,fidelity,entropy,energy_basis
+from Non_observables import zm
+from Construction_functions import bin_to_int_base_m,int_to_bin_base_m,cycle_bits_state
+from Search_functions import find_index_bisection
+from State_Classes import zm_state,sym_state,prod_state,bin_state,ref_state
+from rw_functions import save_obj,load_obj
+from Calculations import level_stats,fidelity,eig_overlap,entropy,site_precession,site_projection,time_evolve_state
+
+N=4
+D=8
 H = common_mpo.PXP(N,"open")
-method = dmrg(H,D)
-psi = method.run()
+N=1000
+method = idmrg(H,2,D)
+psi_trial = method.run(N)
+
+H = common_mpo.PXP(N,"open")
+method = dmrg(H,D,psi=psi_trial)
+psi = method.run(N)
+method.plot_convergence()
+method.plot_var()
