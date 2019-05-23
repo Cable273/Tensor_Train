5d472b03015e3d3b4d7bdf856b54c5f4b164fd9b
Kieran Bull <pykb@localhost.localdomain>
2019-05-23 20:54:30 +0100

    two site (nearest neighbor) trotter gates + time evolution

--- time_evolution.py
+++ time_evolution.py
@@ -10,31 +10,54 @@ from DMRG import dmrg
 from MPS import *
 from compression import *
 from trotter_MPO import *
+def set_steps(trotter,tau):
+    L,W,Q = trotter.mpo(tau)
+    step1=open_MPO(N)
+    step2=open_MPO(N)
+    step3=open_MPO(N)
+    for n in range(0,step1.length-2,3):
+        step1.set_entry(n,L,"right")
+        step1.set_entry(n+1,W,"both")
+        step1.set_entry(n+2,Q,"left")
+
+    for n in range(1,step1.length-4,3):
+        step2.set_entry(n,L,"right")
+        step2.set_entry(n+1,W,"both")
+        step2.set_entry(n+2,Q,"left")
+
+    for n in range(2,step1.length-3,3):
+        step3.set_entry(n,L,"right")
+        step3.set_entry(n+1,W,"both")
+        step3.set_entry(n+2,Q,"left")
+    return step1,step2,step3
+
+def odd_even_gates(trotter,tau):
+    A,B = trotter.mpo(tau)
+    gate1=open_MPO(N)
+    gate2=open_MPO(N)
+    for n in range(0,gate1.length-1,2):
+        gate1.set_entry(n,A,"right")
+        gate1.set_entry(n+1,B,"left")
+    for n in range(1,gate2.length-2,2):
+        gate2.set_entry(n,A,"right")
+        gate2.set_entry(n+1,B,"left")
+    return gate1,gate2
+
+def fourth_order_tau(tau):
+    t1 = 1/(4-np.power(4,1/3))*tau
+    t2 = t1
+    t3 = tau - 2*t1 - 2*t2
+    return t1,t2,t3
+
 tau = 0.1
-N=30
+N=10
 D=2
 Nc=2
-L,W,Q = pcp_trotter_mpo(tau,Nc)
-step1=open_MPO(N)
-step2=open_MPO(N)
-step3=open_MPO(N)
+trotter = two_site_trotter.factory("xx")
+step1,step2 = odd_even_gates(trotter,tau)
+# step1,step2,step3 = set_steps(trotter,tau)
 
-for n in range(0,step1.length-2,3):
-    step1.set_entry(n,L,"right")
-    step1.set_entry(n+1,W,"both")
-    step1.set_entry(n+2,Q,"left")
-
-for n in range(1,step1.length-4,3):
-    step2.set_entry(n,L,"right")
-    step2.set_entry(n+1,W,"both")
-    step2.set_entry(n+2,Q,"left")
-
-for n in range(2,step1.length-3,3):
-    step3.set_entry(n,L,"right")
-    step3.set_entry(n+1,W,"both")
-    step3.set_entry(n+2,Q,"left")
-
-#neel state
+# neel state
 I = np.eye(1)
 A = np.zeros([Nc,1,1])
 A[0] = 1
@@ -51,21 +74,33 @@ for n in range(1,N-2,2):
 for n in range(2,N-1,2):
     psi.set_entry(n,B,"both")
 psi.set_entry(N-1,Vr,"left")
-
 psi_orig = copy.deepcopy(psi)
 
+from Tensor_Train import rail_network
 f = []
 t_max = 20
 t=np.arange(0,t_max,tau)
-no_steps = np.size(t)
 pbar=ProgressBar()
 print("Time evolving")
 errors = []
-for n in pbar(range(0,no_steps)):
+for n in pbar(range(0,np.size(t))):
     overlap = np.abs(psi.dot(psi_orig))**2
     f=np.append(f,overlap)
+
+    # step1.dot(psi)
+    # c = svd_compress(psi,D)
+    # psi = c.compress()
+    # errors = np.append(errors,c.error)
+    # step2.dot(psi)
+    # c = svd_compress(psi,D)
+    # psi = c.compress()
+    # errors = np.append(errors,c.error)
+    # step3.dot(psi)
+    # c = svd_compress(psi,D)
+    # psi = c.compress()
+    # errors = np.append(errors,c.error)
+
     step1.dot(psi)
-    # psi= svd_compress(psi,D).compress(verbose=True)
     c = svd_compress(psi,D)
     psi = c.compress()
     errors = np.append(errors,c.error)
@@ -73,21 +108,8 @@ for n in pbar(range(0,no_steps)):
     c = svd_compress(psi,D)
     psi = c.compress()
     errors = np.append(errors,c.error)
-    # psi= svd_compress(psi,D).compress(verbose=True)
-    step3.dot(psi)
-    c = svd_compress(psi,D)
-    psi = c.compress()
-    errors = np.append(errors,c.error)
-    # psi= svd_compress(psi,D).compress(verbose=True)
-    # print(f[n])
-
-    # psi= svd_compress(psi,D).compress()
-    # compressor = var_compress(psi,D,verbose=True)
-    # compressor = var_compress(psi,D,psi_trial=psi_trial,verbose=True)
-    # psi = compressor.compress(5e-1,max_sweeps=5000)
-
-plt.plot(errors)
-plt.show()
 
 plt.plot(t,f)
+plt.show()
+plt.plot(errors)
 plt.show()
--- trotter_MPO.py
+++ trotter_MPO.py
@@ -21,125 +21,101 @@ from State_Classes import zm_state,sym_state,prod_state,bin_state,ref_state
 from rw_functions import save_obj,load_obj
 from Calculations import level_stats,fidelity,eig_overlap,entropy,site_precession,site_projection,time_evolve_state
 
-# class H_three_site:
-    # def gen(name,Nc=None):
-        # if name is "pxp": return pxp_3site(Nc)
-        # if name is "pcp": return pcp_3site(Nc)
+class two_site_trotter:
+    def factory(name,Nc=None):
+        if name == "xx": return xx_trotter()
+    def mpo(self,tau):
+        dim=np.size(self.H.sector.matrix(),axis=0)
+        U = np.eye(dim) - 1j * tau * self.H.sector.matrix()
+        T = U.reshape(np.array((self.base,self.base,self.base,self.base)))
+        T = np.einsum('ijkl->ikjl',T)
+        shape = np.shape(T)
+        T = T.reshape(np.array((shape[0]*shape[1],shape[2]*shape[3])))
 
-# def pxp_3site(Nc):
-    # pxp = unlocking_System(np.arange(0,Nc),"open",Nc,3)
-    # pxp.gen_basis()
-    # X = spin_Hamiltonian(pxp,"x").site_ops[1]
-    # H = Hamiltonian(pxp)
-    # H.site_ops[1] = X
-    # H.model = np.array([[0,1,0]])
-    # H.model_coef=np.array([[1]])
-    # H.gen()
-    # H.sector.find_eig()
-    # return H.sector.matrix()
+        A,S,B = np.linalg.svd(T,full_matrices=False)
+        A = np.dot(A,np.power(np.diag(S),0.5))
+        B = np.dot(np.power(np.diag(S),0.5),B)
+        A=A.reshape(np.array((shape[0],shape[1],np.size(S))))
 
-# def pcp_3site(Nc):
-    # pxp = unlocking_System(np.arange(0,Nc),"open",Nc,3)
-    # pxp.gen_basis()
-    # C = clock_Hamiltonian(pxp).site_ops[1]
-    # H = Hamiltonian(pxp)
-    # H.site_ops[1] = X
-    # H.model = np.array([[0,1,0]])
-    # H.model_coef=np.array([[1]])
-    # H.gen()
-    # H.sector.find_eig()
-    # return H.sector.matrix()
+        B=B.reshape(np.array((np.size(S),shape[2],shape[3])))
+        B = np.einsum('ijk->jki',B)
+        return A,B
 
-def pxp_trotter_mpo(tau,Nc):
-    pxp = unlocking_System(np.arange(0,Nc),"open",Nc,3)
-    pxp.gen_basis()
-    X = spin_Hamiltonian(pxp,"x").site_ops[1]
-    H = Hamiltonian(pxp)
-    H.site_ops[1] = X
-    H.model = np.array([[0,1,0]])
-    H.model_coef=np.array([[1]])
-    H.gen()
-    H.sector.find_eig()
-    dim=np.size(H.sector.matrix(),axis=0)
-    U = np.eye(dim) - 1j * tau * H.sector.matrix()
-    T = U.reshape(np.array((pxp.base,pxp.base,pxp.base,pxp.base,pxp.base,pxp.base)))
-    T = np.einsum('ijkunm->iujnkm',T)
-    T = T.reshape(np.array((np.power(pxp.base,4),np.power(pxp.base,2))))
-    U,S0,Vh = np.linalg.svd(T,full_matrices=False)
-    from Diagnostics import is_unitary
+class xx_trotter(two_site_trotter):
+    def __init__(self):
+        self.base = 2
+        self.H = self.xx_2site()
 
-    #finalize reshaping Vh
-    O_right = Vh.reshape(np.array((np.size(S0),pxp.base,pxp.base)))
-    O_right = np.einsum('ijk->jki',O_right)
+    def xx_2site(self):
+        pxp = unlocking_System(np.arange(0,self.base),"open",self.base,2)
+        pxp.gen_basis()
+        H = Hamiltonian(pxp)
+        H.site_ops[1] = np.array([[0,1],[1,0]])
+        H.model = np.array([[1,1]])
+        H.model_coef=np.array([[1]])
+        H.gen()
+        H.sector.find_eig()
+        return H
 
-    U = U.reshape(np.array((np.power(pxp.base,2),np.power(pxp.base,2)*np.size(S0))))
-    U,S1,Vh = np.linalg.svd(U,full_matrices=False)
+class three_site_trotter:
+    def factory(name,Nc=None):
+        if name == "pxp": return pxp_trotter(Nc)
+        if name == "pcp": 
+            return pcp_trotter(Nc)
 
-    O_left = U.reshape(np.array((pxp.base,pxp.base,np.size(S1))))
+    def mpo(self,tau):
+        dim=np.size(self.H.sector.matrix(),axis=0)
+        U = np.eye(dim) - 1j * tau * self.H.sector.matrix()
+        T = U.reshape(np.array((self.base,self.base,self.base,self.base,self.base,self.base)))
+        T = np.einsum('ijkunm->iujnkm',T)
+        T = T.reshape(np.array((np.power(self.base,4),np.power(self.base,2))))
+        U,S0,Vh = np.linalg.svd(T,full_matrices=False)
+        from Diagnostics import is_unitary
 
-    Vh = Vh.reshape(np.array((np.size(S1),pxp.base,pxp.base,np.size(S0))))
-    O_mid = np.einsum('ij,jkuv,vn->ikun',np.diag(S1),Vh,np.diag(S0))
-    O_mid = np.einsum('ijku->jkiu',O_mid)
+        #finalize reshaping Vh
+        O_right = Vh.reshape(np.array((np.size(S0),self.base,self.base)))
+        O_right = np.einsum('ijk->jki',O_right)
 
-    return O_left,O_mid,O_right
+        U = U.reshape(np.array((np.power(self.base,2),np.power(self.base,2)*np.size(S0))))
+        U,S1,Vh = np.linalg.svd(U,full_matrices=False)
 
-def pcp_trotter_mpo(tau,Nc):
-    pxp = unlocking_System(np.arange(0,Nc),"open",Nc,3)
-    pxp.gen_basis()
-    C = clock_Hamiltonian(pxp).site_ops[1]
-    H = Hamiltonian(pxp)
-    H.site_ops[1] = C
-    H.model = np.array([[0,1,0]])
-    H.model_coef=np.array([[1]])
-    H.gen()
-    H.sector.find_eig()
-    dim=np.size(H.sector.matrix(),axis=0)
-    U = np.eye(dim) - 1j * tau * H.sector.matrix()
-    T = U.reshape(np.array((pxp.base,pxp.base,pxp.base,pxp.base,pxp.base,pxp.base)))
-    T = np.einsum('ijkunm->iujnkm',T)
-    T = T.reshape(np.array((np.power(pxp.base,4),np.power(pxp.base,2))))
-    U,S0,Vh = np.linalg.svd(T,full_matrices=False)
-    from Diagnostics import is_unitary
+        O_left = U.reshape(np.array((self.base,self.base,np.size(S1))))
 
-    #finalize reshaping Vh
-    O_right = Vh.reshape(np.array((np.size(S0),pxp.base,pxp.base)))
-    O_right = np.einsum('ijk->jki',O_right)
+        Vh = Vh.reshape(np.array((np.size(S1),self.base,self.base,np.size(S0))))
+        O_mid = np.einsum('ij,jkuv,vn->ikun',np.diag(S1),Vh,np.diag(S0))
+        O_mid = np.einsum('ijku->jkiu',O_mid)
+        return O_left,O_mid,O_right
 
-    U = U.reshape(np.array((np.power(pxp.base,2),np.power(pxp.base,2)*np.size(S0))))
-    U,S1,Vh = np.linalg.svd(U,full_matrices=False)
+class pcp_trotter(three_site_trotter):
+    def __init__(self,Nc):
+        self.base = Nc
+        self.H = self.pcp_3site(Nc)
 
-    O_left = U.reshape(np.array((pxp.base,pxp.base,np.size(S1))))
+    def pcp_3site(self,Nc):
+        pxp = unlocking_System(np.arange(0,self.base),"open",self.base,3)
+        pxp.gen_basis()
+        C = clock_Hamiltonian(pxp).site_ops[1]
+        H = Hamiltonian(pxp)
+        H.site_ops[1] = C
+        H.model = np.array([[0,1,0]])
+        H.model_coef=np.array([[1]])
+        H.gen()
+        H.sector.find_eig()
+        return H
 
-    Vh = Vh.reshape(np.array((np.size(S1),pxp.base,pxp.base,np.size(S0))))
-    O_mid = np.einsum('ij,jkuv,vn->ikun',np.diag(S1),Vh,np.diag(S0))
-    O_mid = np.einsum('ijku->jkiu',O_mid)
+class pxp_trotter(three_site_trotter):
+    def __init__(self,Nc):
+        self.base = Nc
+        self.H = self.pxp_3site(Nc)
 
-    return O_left,O_mid,O_right
-
-# def free_para(tau):
-    # pxp = unlocking_System([0,1],"open",2,3)
-    # pxp.gen_basis()
-    # H = spin_Hamiltonian(pxp,"x")
-    # H.gen()
-    # H.sector.find_eig()
-    # H_diag = np.exp(-1j*tau*np.diag(H.sector.eigvalues()))
-    # U = np.dot(H.sector.eigvectors(),np.dot(H_diag,np.conj(np.transpose(H.sector.eigvectors()))))
-    # T = U.reshape(np.array((pxp.base,pxp.base,pxp.base,pxp.base,pxp.base,pxp.base)))
-    # T = np.einsum('ijkunm->iujnkm',T)
-    # T = T.reshape(np.array((np.power(pxp.base,4),np.power(pxp.base,2))))
-    # U,S0,Vh = np.linalg.svd(T,full_matrices=False)
-
-    # #finalize reshaping Vh
-    # O_right = Vh.reshape(np.array((np.size(S0),pxp.base,pxp.base)))
-    # O_right = np.einsum('ijk->jki',O_right)
-
-    # U = U.reshape(np.array((np.power(pxp.base,2),np.power(pxp.base,2)*np.size(S0))))
-    # U,S1,Vh = np.linalg.svd(U,full_matrices=False)
-
-    # O_left = U.reshape(np.array((pxp.base,pxp.base,np.size(S1))))
-
-    # Vh = Vh.reshape(np.array((np.size(S1),pxp.base,pxp.base,np.size(S0))))
-    # O_mid = np.einsum('ij,jkuv,vn->ikun',np.diag(S1),Vh,np.diag(S0))
-    # O_mid = np.einsum('ijku->jkiu',O_mid)
-
-    # return O_left,O_mid,O_right
+    def pxp_3site(self,Nc):
+        pxp = unlocking_System(np.arange(0,self.base),"open",self.base,3)
+        pxp.gen_basis()
+        X = spin_Hamiltonian(pxp,"x").site_ops[1]
+        H = Hamiltonian(pxp)
+        H.site_ops[1] = X
+        H.model = np.array([[0,1,0]])
+        H.model_coef=np.array([[1]])
+        H.gen()
+        H.sector.find_eig()
+        return H
